350927baac6b6ba591aa5160150d06a6821ed306
Prashant <prashant.p@simformsolutions.com>
2023-05-18 14:29:39 +0530

    api fetch

--- index.js
+++ index.js
@@ -1,40 +1,74 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import logger from 'redux-logger';
+import axios from 'axios';
+import thunk from 'redux-thunk';
 
+const init = 'init'
 const inc = 'increment'
 const dec = 'decrement'
 const incByAmt = 'incrementByAmt'
 
 //store
-const store = createStore(reducer, applyMiddleware(logger.default));
+const store = createStore(
+    combineReducers({
+        account: accountReducer,
+        bonus: bonusReducer
+    }),
+    applyMiddleware(logger.default, thunk.default)
+);
 
 // const history = []
 //reducer
 
 
 
-function reducer(state = { amount: 1 }, action) {
-    if (action.type === inc) {
-        // immutability
-        return { amount: state.amount + 1 }
+function accountReducer(state = { amount: 1 }, action) {
+    switch (action.type) {
+        case init:
+            return { amount: action.payload }
+        case inc:
+            return { amount: state.amount + 1 }
+        case dec:
+            return { amount: state.amount - 1 }
+        case incByAmt:
+            return { amount: state.amount + action.payload }
+        default:
+            return state
     }
-    if (action.type === dec) {
-        return { amount: state.amount - 1 }
+}
+
+function bonusReducer(state = {points: 0}, action){
+    switch (action.type){
+        case inc:
+            return { points: state.points +1}
+        default: 
+            return state
     }
-    if (action.type === incByAmt){
-        return {amount : state.amount + action.payload}
-    }
-    return state
 }
 
 // store.subscribe(() => {
 //     history.push(store.getState())
 //     console.log(history);
 // })
+// async api call
+// async function getUser() {
+//     store.dispatch(initUser(data.amount))
+// }
 
 //action functions
+function getUser(id) {
+    return async (dispatch) => {
+        const { data } = await axios.get(`http://localhost:3000/Accounts/${id}`)
+        dispatch(initUser(data.Amount))
+    }
+}
+
+function initUser(value) {
+    return { type: init, payload: value }
+}
+
 function increment() {
-    return{type: inc}
+    return { type: inc }
 }
 function decrement() {
     return { type: dec }
@@ -43,10 +77,13 @@ function incrementByAmount(value) {
     return { type: incByAmt, payload: value }
 }
 
-
+// getUser()
 // action
-setInterval(() => {
+setTimeout(() => {
+    // store.dispatch(getUser(1))
+    store.dispatch(getUser(2))
     // store.dispatch(increment())
-    store.dispatch(decrement())
-    // store.dispatch(incrementByAmount(3))
 }, 2000)
+
+
+
